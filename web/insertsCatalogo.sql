e9c84f53aa67605edddc118fbd82f6c871ce652a
YHOAN CUCAITA <YHOAN CUCAITA@192.168.1.2>
2015-09-20 17:00:04 -0500

    se agrega archivo sql

--- web/insertsCatalogo.sql
+++ web/insertsCatalogo.sql
@@ -1,3 +1,10 @@
+create table ROOT.CATALOGO(
+    Id numeric primary key not null,
+    Nombre varchar(100),
+    Precio double not null,
+    Imagen varchar(300)
+)
+
 insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(1,'Bicicleta BMX HARO',550000,'bici1.png');
 insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(2,'Bicicleta BMX Bianchi',550000,'bici2.png');
 insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(3,'Bicicleta BMX felt',750000,'bici3.png');
