7ea8b008a2d45f9fbd7e25bdcc6b63b4f7440872
YHOAN CUCAITA <YHOAN CUCAITA@192.168.1.2>
2015-09-20 16:48:23 -0500

    se agrega archivo sql

---
+++ web/insertsCatalogo.sql
@@ -0,0 +1,5 @@
+insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(1,'Bicicleta BMX HARO',550000,'bici1.png');
+insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(2,'Bicicleta BMX Bianchi',550000,'bici2.png');
+insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(3,'Bicicleta BMX felt',750000,'bici3.png');
+insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(4,'Bicicleta BMX alu',850000,'bici4.png');
+insert into ROOT.CATALOGO (ID,NOMBRE,PRECIO,IMAGEN) values(5,'Bicicleta BMX GT',2100000,'bici5.png');
